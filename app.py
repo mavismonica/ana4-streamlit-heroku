1fdc46cab48319994a8f8518bc0ed38f67cdbbaa
mavismonica <61350590+mavismonica@users.noreply.github.com>
2021-07-25 22:47:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import tensorflow as tf
 import streamlit as st
 @st.cache(allow_output_mutation=True)
-def load_model():
-  model=tf.keras.models.load_model('VGG_model.hdf5')
-  return model
+#def load_model():
+ #
+#   return model
 with st.spinner('Model is being loaded..'):
-  model=load_model()
+#   model=load_model()
+  model=tf.keras.models.load_model('VGG_model.hdf5')
+
 st.write("""
          # Grocery Classification
          """
