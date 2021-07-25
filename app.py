133746bbbfbe9d2cfdf33e5ff50dec1fc6c89f43
mavismonica <61350590+mavismonica@users.noreply.github.com>
2021-07-25 15:41:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,13 @@ def import_and_predict(image_data, model):
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img_resize = (cv2.resize(img, dsize=(250, 250),interpolation=cv2.INTER_CUBIC))/255.
-        img_reshape = img_resize[np.newaxis,...]
-        prediction = model.predict(img_reshape)
+        img = img.reshape(1, 250, 250, 1)
+        img = img.astype('float32')
+        img = img / 255.0
+        # img_resize = (cv2.resize(img, dsize=(250, 250),interpolation=cv2.INTER_CUBIC))/255.
+        # img_reshape = img_resize[np.newaxis,...]
+        
+        prediction = model.predict(img)
         return prediction
 if file is None:
     st.text("Please upload an image file")
