0416111251d2f1b511aa2e9c60bfaad1f042de4a
mavismonica <61350590+mavismonica@users.noreply.github.com>
2021-07-27 14:48:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import streamlit as st
 @st.cache(allow_output_mutation=True)
 def load_model():
- model=tf.keras.models.load_model('VGG_model.hdf5')
+ model=tf.keras.models.load_model('./VGG_model.hdf5')
  return model
 
 with st.spinner('Model is being loaded..'):
